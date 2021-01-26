1131f6f38a42026467aa5f3431ec7f69b5ebf21a
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-01-26 10:31:25 -0300

    deploy da primeira aulas

---
+++ src/components/QuizBackground/index.js
@@ -0,0 +1,35 @@
+// src/components/QuizBackground/index.js
+import styled from 'styled-components';
+
+
+const QuizBackground = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center;    
+  background-image: url(/game.jpg);
+  background-color: ${({ theme }) => theme.colors.mainBg};
+  flex: 1;
+  @media screen and (max-width: 500px) {
+    
+    &:after {
+      content: "";
+      background-size: cover;
+    background-position: center;
+      background-image: url(/game.jpg);;
+      display: block;
+      width: 106%;
+      height: 106%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 1;
+    }
+    *:first-child {
+      position: relative;
+      z-index: 10;
+    }
+  }
+`;
+
+export default QuizBackground;
