dc567ae74c0dbf3e994f155504248701ea7e593b
RafaelSouzaJava <rafael-souza4@outlook.com>
2021-01-28 15:55:31 -0300

    Ajustes para liberar api publica

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo';
+
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
@@ -40,7 +40,6 @@ export default function Home() {
         <title>Geeks Quiz</title>
       </Header>
       <QuizContainer>
-        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
